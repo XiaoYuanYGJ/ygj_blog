b43cfe678f0f7842353f8b0d248ee513f2ac7f54
xiaohe <xiaohe@xiaohe-PC>
2019-01-24 12:09:05 +0800

    系统验证码

--- ygj-blog/src/main/java/com/ygj/blog/MyBlogApplication.java
+++ ygj-blog/src/main/java/com/ygj/blog/MyBlogApplication.java
@@ -1,7 +1,10 @@
 package com.ygj.blog;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * 
@@ -9,6 +12,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2019年1月20日
  */
 @SpringBootApplication
+@MapperScan("com.ygj.blog.dao") //由于不能自动创建dao
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class MyBlogApplication {
 
 	public static void main(String[] args) {
